ee508bada20ac5afbffa5ae8f7f1d8d92549796a
Tuuu <tu@techmaster.vn>
2016-12-21 17:21:58 +0700

    //init Movies

---
+++ movieapp-master/src/app.ios.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+import { registerScreens } from './screens';
+
+import { iconsMap, iconsLoaded } from './utils/AppIcons';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+registerScreens(store, Provider);
+
+const navigatorStyle = {
+	navBarTranslucent: true,
+	drawUnderNavBar: true,
+	navBarTextColor: 'white',
+	navBarButtonColor: 'white',
+	statusBarTextColorScheme: 'light',
+};
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		iconsLoaded.then(() => {
+			this.startApp();
+		});
+	}
+
+	startApp() {
+		Navigation.startTabBasedApp({
+			tabs: [
+				{
+					label: 'Movie',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-film-outline'],
+					selectedIcon: iconsMap['ios-film'],
+					title: 'Movies',
+					navigatorStyle
+				},
+				{
+					label: 'TV Shows',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-desktop-outline'],
+					selectedIcon: iconsMap['ios-desktop'],
+					title: 'TV Shows',
+					navigatorStyle
+				}
+			],
+			tabsStyle: {
+				tabBarButtonColor: 'white',
+				tabBarSelectedButtonColor: 'white',
+				tabBarBackgroundColor: 'green'
+			}
+		});
+	}
+}
+
+export default App;
---
+++ movieapp-master/src/constants/api.js
@@ -0,0 +1,6 @@
+export const TMDB_URL = 'https://api.themoviedb.org/3';
+export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p';
+export const TMDB_API_KEY = 'f639f1e93d3e52b43b8eb4621a1c6694';
+
+export const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/videos';
+export const YOUTUBE_API_KEY = 'AIzaSyBIEooN2ThqWsHc5GRRLRP6SGyWMeoBzq8';
